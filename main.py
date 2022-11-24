c716a5e865359d48ab544a69c6819a5d29117900
Ravazzi <henriqueravazzi3@gmail.com>
2022-11-23 21:58:09 -0300

    Fifth Criteria Update

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ alternatives = [
     'First Alternative',
     'Second Alternative',
     'Third Alternative',
-    'Fourth Alternative'
+    'Fourth Alternative',
+    'Fifth Alternative'
 ]
 
 my_decision = CriteriasPriorization(alternatives)
@@ -14,12 +15,14 @@ first_criteria_values = [10, 22, 14, 15]
 second_criteria_values = [9, 98, 14, 15]
 third_criteria_values = [10, 30, 14, 15]
 fourth_criteria_value = [5, 83, 14, 15]
+fifth_criteria_value = [15, 20, 25, 30]
 
 values = np.array([
     first_criteria_values,
     second_criteria_values,
     third_criteria_values,
-    fourth_criteria_value
+    fourth_criteria_value,
+    fifth_criteria_value
 ])
 
 output = my_decision.prioritize(values)
